f18e2f417e5c404c225a57ce306a934dee4852a8
RobTecMx <rleyv@tec.mx>
2022-03-28 18:24:13 -0600

    Cambio realizado

---
+++ gitcommit.sh
@@ -0,0 +1,15 @@
+
+MSSG='Cambio realizado'
+User='robtecmx'
+Token='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'
+
+git add .
+
+git commit -m "$MSSG"
+
+git remote add origin https://github.com/robtecmx/TC3005B.git
+
+git push origin master
+
+echo '$User'
+echo '$Token'
